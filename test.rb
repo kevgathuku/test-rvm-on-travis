82d5c74b96092061373163e2d2c71c8d531296dd
Prem Sichanugrist <s@sikac.hu>
2016-01-08 16:34:00 -0500

    Update test code

--- test.rb
+++ test.rb
@@ -1,1 +1,15 @@
-puts RUBY_VERSION
+# encoding: UTF-8
+
+RUBY_VERSIONS = %(
+  1.9.3
+  2.0.0
+  2.1.8
+  2.2.4
+  2.3.0
+  2.4.0
+)
+
+success = RUBY_VERSIONS.include?(RUBY_VERSION)
+
+puts "#{RUBY_VERSION} #{success ? "✅" : "❌"}"
+exit(success ? 0 : 1)
